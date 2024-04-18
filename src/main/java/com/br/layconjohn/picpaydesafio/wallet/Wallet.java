fe7fd12b1666dd7e4d1caa22b60933deca9d41f3
Laycon John <laycon.gomes@tld.com.br>
2024-04-18 17:40:25 -0300

    feat: Wallet record

---
+++ src/main/java/com/br/layconjohn/picpaydesafio/wallet/Wallet.java
@@ -0,0 +1,19 @@
+package com.br.layconjohn.picpaydesafio.wallet;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("WALLET")
+public record Wallet(
+    @Id Long id,
+    String fullname,
+    Long cpf,
+    String email,
+    String password,
+    int type,
+    BigDecimal balance
+) {
+    
+}
