25f6971e14ab0a5f3c2421499745a247238ea979
Laycon John <laycon.gomes@tld.com.br>
2024-04-18 17:46:26 -0300

    feat: Wallet type enum

---
+++ src/main/java/com/br/layconjohn/picpaydesafio/wallet/WalletType.java
@@ -0,0 +1,15 @@
+package com.br.layconjohn.picpaydesafio.wallet;
+
+public enum WalletType {
+    COMUM(1), LOJISTA(2);
+
+    private int value;
+
+    private WalletType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
