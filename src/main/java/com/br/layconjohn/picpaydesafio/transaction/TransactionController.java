f9f88137d253898e4b45d2cc606566f333dbd10d
Laycon John <laycon.gomes@tld.com.br>
2024-04-19 15:05:39 -0300

    feat: transaction controller

---
+++ src/main/java/com/br/layconjohn/picpaydesafio/transaction/TransactionController.java
@@ -0,0 +1,26 @@
+package com.br.layconjohn.picpaydesafio.transaction;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("transaction")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return this.transactionService.create(transaction);
+    }
+
+
+}
