59096ae1e2bf884a5910e10f2a398caa2bdb1a8d
Laycon John <laycon.gomes@tld.com.br>
2024-04-19 15:53:08 -0300

    feat: insert Logger on notification consumer

--- src/main/java/com/br/layconjohn/picpaydesafio/notification/NotificationConsumer.java
+++ src/main/java/com/br/layconjohn/picpaydesafio/notification/NotificationConsumer.java
@@ -1,13 +1,19 @@
 package com.br.layconjohn.picpaydesafio.notification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import com.br.layconjohn.picpaydesafio.authorization.AuthorizerService;
 import com.br.layconjohn.picpaydesafio.transaction.Transaction;
 
 @Service
 public class NotificationConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizerService.class);
+
+
     private RestClient restClient;
 
     public NotificationConsumer(RestClient.Builder builder) {
@@ -18,6 +24,8 @@ public class NotificationConsumer {
 
     @KafkaListener(topics = "transaction-notification", groupId = "picpay-desafio")
     public void receiveNotification(Transaction transaction) {
+        LOGGER.info("Notifying transaction: {}", transaction);
+
         var response = this.restClient.get()
         .retrieve()
         .toEntity(Notification.class);
@@ -25,5 +33,7 @@ public class NotificationConsumer {
         if (response.getStatusCode().isError() || !response.getBody().message()) {
             throw new NotificationException("Error to send notification");
         }
+
+        LOGGER.info("Notification has been send: {}", transaction);
     }
 }
